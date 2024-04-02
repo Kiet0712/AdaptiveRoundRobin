3e9efb8d2de77fd13c2764e45208bf01912b23b3
Kiet0712 <kiet.dangvutuan0712@hcmut.edu.vn>
2024-04-02 20:06:22 +0700

    fix bug

--- cal_quantum.py
+++ cal_quantum.py
@@ -8,7 +8,7 @@ def average_burst(burst_time_array: np.ndarray):
 def average_max_burst(burst_time_array: np.ndarray):
     return (np.mean(burst_time_array)+np.max(burst_time_array))/2
 def sqrt_median_HB_burst(burst_tim_array: np.ndarray):
-    return np.sqrt(np.mean(burst_tim_array)*np.max(burst_tim_array))
+    return np.sqrt(np.median(burst_tim_array)*np.max(burst_tim_array))
 def average_median_burst(burst_time_array: np.ndarray):
     return (np.mean(burst_time_array)+np.median(burst_time_array))/2
 def sqrt_average_median_burst(burst_time_array: np.ndarray):
