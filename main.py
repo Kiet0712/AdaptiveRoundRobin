fix and add response time variance

main.py
@@ -25,7 +25,8 @@ DATA_LIST ={
     'avg_turnaround_time' : [],
     'avg_waiting_time' : [],
     'avg_response_time' : [],
-    'context_swiches' : []
+    'context_swiches' : [],
+    'response_time_var' : []
 }
 
 
@@ -37,7 +38,6 @@ Number_of_Process = 10
 for i in range(Number_of_Process):
     field_names.append('P'+ str(i+1))
 
-print(field_names)
 
 mydata = []
 
@@ -50,18 +50,20 @@ for i in range(Number_of_Process):
         'avg_turnaround_time' : new_row.copy(),
         'avg_waiting_time' : new_row.copy(),
         'avg_response_time' : new_row.copy(),
-        'context_swiches' : new_row.copy()
+        'context_swiches' : new_row.copy(),
+        'response_time_var' : new_row.copy()
     }
     for method in METHOD_ZOO:
         cal_quantum,round_robin = METHOD_ZOO[method]
         quantum_time = np.int32(cal_quantum(burst_time_array))
-        avg_turnaround_time, avg_waiting_time, avg_response_time,context_swiches = round_robin(processes, quantum_time)
+        avg_turnaround_time, avg_waiting_time, avg_response_time,context_swiches,response_time_var = constant_round_robin(processes,quantum_time)
         ROW['avg_turnaround_time'].update({method : avg_turnaround_time})
         ROW['avg_waiting_time'].update({method : avg_waiting_time})
         ROW['avg_response_time'].update({method : avg_response_time})
         ROW['context_swiches'].update({method : context_swiches})
+        ROW['response_time_var'].update({method : response_time_var})
 
-        new_row.update({method : (avg_turnaround_time,avg_waiting_time,avg_response_time,context_swiches)})
+        new_row.update({method : (avg_turnaround_time,avg_waiting_time,avg_response_time,context_swiches,response_time_var)})
         #print(method+ ' method:')
         #print("Average Turnaround Time:", avg_turnaround_time)
         #print("Average Waiting Time:", avg_waiting_time)

sampling.py
@@ -7,11 +7,12 @@ def sampling_process(
         std_burst_time: float,
         mean_arrival_time: float,
         std_arrival_time: float,
-        num_process: int
+        num_process: int,
+        field_names: list
     ) -> list[tuple]:
-    burst_time_array = np.exp(np.int32(np.random.normal(loc=mean_burst_time,scale=std_burst_time,size=num_process)))
-    arrival_time_array = np.exp(np.int32(np.random.normal(loc=mean_arrival_time,scale=std_arrival_time,size=num_process)))
+    burst_time_array = np.int32(np.exp(np.random.normal(loc=mean_burst_time,scale=std_burst_time,size=num_process)))
+    arrival_time_array = np.int32(np.exp(np.random.normal(loc=mean_arrival_time,scale=std_arrival_time,size=num_process)))
     result_sampling = []
     for i in range(num_process):
         result_sampling.append((i,arrival_time_array[i],burst_time_array[i]))
-    return result_sampling,burst_time_array,arrival_time_array
+    return result_sampling,burst_time_array,arrival_time_array,dict(zip(field_names,burst_time_array)) 

standard_round_robin.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def constant_round_robin(processes, time_quantum):
     ready_queue = []  # Queue to hold ready processes
     turnaround_time = [0] * len(processes)  # Time from submission to completion
@@ -42,7 +44,7 @@ def constant_round_robin(processes, time_quantum):
     avg_waiting_time = sum(waiting_time) / len(processes)
     avg_response_time = sum(response_time) / len(processes)
 
-    return avg_turnaround_time, avg_waiting_time, avg_response_time, context_switches
+    return avg_turnaround_time, avg_waiting_time, avg_response_time, context_switches,round(np.var(np.array(response_time)),2)
 # processes,burst_time_array,arrival_time_array = sampling_process(
 #     10,4,0,0,10
 # )