eb85c954dafc6bad7ef8f5728f7048f258c83375
unknown <kiendepzai3@gmail.com>
2024-04-08 00:22:20 +0700

    arrival time !=0 , adding process information

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ for i in range(NUMBER_OF_DATA_POINT):
     with open('process_information.csv','a') as file:
         writer = csv.writer(file)
         writer.writerow(['Test' + str(i) +':'])
-    data = {'Processes' : field_names ,'Burst time':burst_time_array , 'Arrival time':arrival_time_array}
+    data = {'Processes' : field_names ,'Burst time': burst_time_array , 'Arrival time':arrival_time_array}
     data = pd.DataFrame(data)
     data.to_csv('process_information.csv',mode='a',header=True,index=False)
 
